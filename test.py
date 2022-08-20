6887e89b2cfaa9ba32cb5bf4a51eef9a62d37011
Sudarshankgupta <sudarshan.gupta@okstate.edu>
2022-08-20 17:29:08 -0500

    testing python and git

---
+++ test.py
@@ -0,0 +1,15 @@
+print(123, end="\n\n")
+print('abc')
+
+a = 123
+b = 567
+c = 'z'
+
+import pandas as pd
+print(pd.__version__)
+
+result = 0
+print(result)
+
+for i in [a, b, c]:
+    result = result + i
